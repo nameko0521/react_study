350569d3f041aa8554d9d6fd9c79348a83f72481
nameko0521 <60539693+nameko0521@users.noreply.github.com>
2022-01-11 13:42:24 +0900

    We learned about global state management

---
+++ 5_globalstatemanagement/src/App.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
+import { Card } from "./components/Card";
+
+export const App = () => {
+    const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
+
+    const onClickSwitch = () => setIsAdmin(!isAdmin);
+
+    return (
+        <div>
+            {isAdmin ? <span>管理者です。</span> : <span>管理者以外です。</span>}
+            <button onClick={onClickSwitch}>Switch</button>
+            <Card isAdmin={isAdmin}/>
+        </div>
+    );
+};
