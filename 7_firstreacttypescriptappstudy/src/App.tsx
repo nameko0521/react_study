126ea1e01b8fb438f6822364a662bbb3082dceba
nameko0521 <60539693+nameko0521@users.noreply.github.com>
2022-01-13 23:13:01 +0900

    とりあえず

--- 7_firstreacttypescriptappstudy/src/App.tsx
+++ 7_firstreacttypescriptappstudy/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ListItem } from "./components/ListItem";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const [users, setUsers] = useState([]);
 
-export default App;
+    useEffect(() => {
+        axios.get("https://example.com/users").then((res) => {
+            setUsers(res.data);
+        })
+    }, []);
+
+    return (
+        <div>
+            {users.map(user => (
+                <ListItem id={user.id} name={user.nama} age={user.age} />
+            ))}
+        </div>
+    );
+};
